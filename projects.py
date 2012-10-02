704e630955ac76b25d56b53fbd6fe0fb17676f6d
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2012-10-02 00:15:16 -0300

    typo

--- projects.py
+++ projects.py
@@ -242,7 +242,7 @@ class Project(object):
         self.repo = hg.repository(self.ui, self.path)
         hgrc = os.path.join(self.path, '.hg', 'hgrc')
         if os.path.exists(hgrc):
-            self.ui.readconfig(hgrc, Trust=True)
+            self.ui.readconfig(hgrc, trust=True)
         self.ctx = self.repo[None]
 
     @locked_cached_property
